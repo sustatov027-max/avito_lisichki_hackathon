7da321d4669956f315b046b62b50608863a3a2aa
millbi <poletama@yandex.ru>
2026-08-03 23:04:01 +0300

    Настройка линтера

--- backend/internal/app/builder.go
+++ backend/internal/app/builder.go
@@ -5,19 +5,20 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/sustatov027-max/avito_lisichki_hackathon/backend/internal/platform/health"
 )
 
 func New(_ context.Context) (*App, error) {
-
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /health", health.HealthHandler)
 	server := &http.Server{
-		Addr:    "localhost:8080",
-		Handler: mux,
+		Addr:              "localhost:8080",
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
 	}
 
 	app := &App{
