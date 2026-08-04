3806d4081487599d0ecc38181339fc803fbc0c19
IvanSuslov <sustatov027@gmail.com>
2026-08-04 18:23:55 +0300

    Изменение настроек CORS

--- backend/internal/app/routes.go
+++ backend/internal/app/routes.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/sustatov027-max/avito_lisichki_hackathon/backend/internal/exchange/repository/postgres"
@@ -12,7 +14,24 @@ import (
 func (a *App) registerRoutes() *gin.Engine {
 	router := gin.Default()
 
-	router.Use(cors.Default())
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+
+		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"},
+
+		AllowHeaders: []string{
+			"Origin",
+			"Content-Type",
+			"Accept",
+			"Authorization",
+			"Idempotency-Key",
+			"X-Requested-With",
+		},
+
+		ExposeHeaders: []string{"Content-Length", "Content-Type"},
+
+		MaxAge: 12 * time.Hour,
+	}))
 
 	exchangeRepository := postgres.NewTradeOfferRepository(a.db)
 	exchangeService := service.NewExchangeService(exchangeRepository)
@@ -24,6 +43,9 @@ func (a *App) registerRoutes() *gin.Engine {
 	}
 
 	router.GET("/health", health.HealthHandler)
+	router.OPTIONS("/*path", func(c *gin.Context) {
+		c.Status(204)
+	})
 
 	return router
 }
