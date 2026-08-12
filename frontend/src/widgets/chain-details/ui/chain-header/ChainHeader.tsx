2b8ef6a87aa39fabfff40f80f81af36a8c678e63
Muhammad Tsaroev <tsaroev17@gmail.com>
2026-08-12 21:35:29 +0300

    add user action required state

--- frontend/src/widgets/chain-details/ui/chain-header/ChainHeader.tsx
+++ frontend/src/widgets/chain-details/ui/chain-header/ChainHeader.tsx
@@ -7,13 +7,35 @@ import type { ChainHeaderProps } from './ChainHeader.types'
 const ChainHeader = (props: ChainHeaderProps) => {
 	const { chain } = props
 
+	if (!chain) {
+		return null
+	}
+
+	const decisionLabels = {
+		pending: 'Решение не принято',
+		accepted: 'Вы приняли цепочку',
+		rejected: 'Вы отклонили цепочку'
+	} as const
+
 	return (
-		chain && (
-			<div className={styles.header}>
+		<div className={styles.header}>
+			<div className={styles.info}>
 				<ChainStatus status={chain.status} />
-				<ChainTimer seconds={chain?.time_left_seconds} />
+				<p
+					className={
+						chain.my_summary.user_action_required
+							? styles.actionRequired
+							: styles.decision
+					}
+				>
+					{chain.my_summary.user_action_required
+						? 'Требуется ваше решение'
+						: decisionLabels[chain.my_summary.my_decision]}
+				</p>
 			</div>
-		)
+
+			<ChainTimer seconds={chain.time_left_seconds} />
+		</div>
 	)
 }
 
